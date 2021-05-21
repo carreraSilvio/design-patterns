cc2b71c401776b170dd12dc246e3da617e35bb03
Silvio Carrera <carrera.silvio@gmail.com>
2021-05-21 15:15:22 -0300

    Implemented flyweight pattern

---
+++ 02 - Flyweight/Scripts/Cube.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public CubeData data;
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Clicked {data.displayName}! Info: {data.info}");
+    }
+}
---
+++ 02 - Flyweight/Scripts/CubeData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(CubeData), menuName = "Data/Flyweight/" + nameof(CubeData))]
+public class CubeData : ScriptableObject
+{
+    public string displayName;
+    [TextArea(10, 10)]
+    public string info;
+}
