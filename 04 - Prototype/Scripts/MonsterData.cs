00fa5230487fc5eb68c8b94fdb8318e71a52ac64
Silvio Carrera <carrera.silvio@gmail.com>
2021-05-29 11:19:47 -0300

    Implemented prototype pattern example

---
+++ 04 - Prototype/Scripts/MonsterData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace PrototypeDesignPattern
+{
+    [CreateAssetMenu(menuName = nameof(PrototypeDesignPattern) + "/" + nameof(MonsterData))]
+    public class MonsterData : MonsterPrototypeData
+    {
+        public MonsterPrototypeData prototypeData;
+    }
+
+}
---
+++ 04 - Prototype/Scripts/MonsterPrototypeData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PrototypeDesignPattern
+{
+    [CreateAssetMenu(menuName = nameof(PrototypeDesignPattern) + "/" + nameof(MonsterPrototypeData))]
+    public class MonsterPrototypeData : ScriptableObject
+    {
+        public new string name;
+        public int health;
+        public int damage;
+    }
+
+}
